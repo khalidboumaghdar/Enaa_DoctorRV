77fe906efffc402acd529d9d052066ad55c3e63b
khalidboumaghdar <137784954+khalidboumaghdar@users.noreply.github.com>
2025-03-04 02:27:50 +0100

    mysql

--- DoctorRV.sql
+++ DoctorRV.sql
@@ -5,15 +5,28 @@ create table patients(
 id int auto_increment primary key,
 nom varchar(40),
 email varchar(40),
-telephone varchar(40)
+telephone varchar(40),
+role ENUM('Admin', 'Medecin', 'Patient') DEFAULT 'Patient'
 );
 create table medecin(
 id int auto_increment primary key,
 nom varchar(40),
 email varchar(40),
-telephone varchar(40)
+telephone varchar(40),
+role ENUM('Admin', 'Medecin', 'Patient') DEFAULT 'Medecin'
 
 );
+create table login(
+id int auto_increment primary key,
+nom varchar(40),
+email varchar(50) unique,
+password varchar(255),
+role ENUM('Admin', 'Médecin', 'Patient') DEFAULT 'Patient'
+
+
+);
+select * from login;
+
 create table rendezvous(
 id int auto_increment primary key,
 datet Date,
